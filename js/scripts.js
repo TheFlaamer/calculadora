99f390093183de8059afcce7e5c5d5a9931f724a
TheFlaamer <megacraftbrsuporte@gmail.com>
2021-06-24 16:18:44 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+const tela = document.querySelector("#tela");
+const botaoNumbero = document.querySelectorAll(".btnNumber");
+const btnDelete = document.querySelector("#btnApagar");
+const btnOpcao = document.querySelectorAll(".btnOpcao");
+let operacao = 0;
+const igual = document.querySelector("#btnPesquisa");
+let parteUm = 0;
+
+botaoNumbero.forEach((el, i) => {
+    el.addEventListener("click", () => {
+        tela.value += el.value;
+    })
+})
+btnDelete.addEventListener("click", () => {
+    const conteudo = tela.value.split("");
+    conteudo.splice(conteudo.length - 1, 1);
+    tela.value = conteudo.join('');
+})
+btnOpcao.forEach((el, i) => {
+    el.addEventListener("click", () => {
+        switch (el.id) {
+            case "btnMais":
+                tela.value += "+"
+                operacao = 0;
+                break;
+            case "btnMenos":
+                tela.value += "-"
+                operacao = 1;
+                break;
+            case "btnMultiplica":
+                tela.value += "*"
+                operacao = 2;
+                break;
+            case "btnDivide":
+                tela.value += "/"
+                operacao = 3;
+                break;
+        }
+    })
+})
+let resultado = 0;
+let splitado = 0;
+let numConvertido = 0;
+igual.addEventListener("click", () => {
+    switch (operacao) {
+        case 0:
+            splitado = tela.value.split("+")
+            numConvertido = converterPNumero(splitado)
+            resultado = numConvertido.reduce((ac, a) => { return ac + a })
+            break;
+        case 1:
+            splitado = tela.value.split("-")
+            numConvertido = converterPNumero(splitado)
+            resultado = numConvertido.reduce((ac, a) => { return ac - a })
+            break;
+        case 2:
+            splitado = tela.value.split("*")
+            numConvertido = converterPNumero(splitado)
+            resultado = numConvertido.reduce((ac, a) => { return ac * a })
+            break;
+        case 3:
+            splitado = tela.value.split("/")
+            numConvertido = converterPNumero(splitado)
+            resultado = numConvertido.reduce((ac, a) => { return ac / a })
+            break;
+    }
+    tela.value = resultado;
+})
+function converterPNumero(array) {
+    let newArr = []
+    array.forEach((el, i) => {
+        newArr.push(Number(el));
+    })
+    return newArr;
+}
